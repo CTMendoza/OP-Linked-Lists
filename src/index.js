98283aef6f2771ff073a32df1d72dec31d5bd1eb
Christopher Mendoza <tiptop1112@gmail.com>
2025-03-27 11:34:12 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+import { loadHomePage } from "./homepage";
+import { menu } from "./menu";
+import { loadContactsPage } from "./contact";
+
+loadHomePage();
+
+const content = document.getElementById('content');
+const homeButton  = document.getElementById('home');
+const menuButton = document.getElementById('menu');
+const contactButton = document.getElementById('contact');
+
+homeButton.addEventListener('click', () => {
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+    loadHomePage();
+});
+
+menuButton.addEventListener('click', () => {
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+    menu();
+});
+
+contactButton.addEventListener('click', () => {
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+    loadContactsPage();
+});
